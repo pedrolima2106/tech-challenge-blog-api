39763ca5cc38265ae1995baf34fc96071ea80b81
pedrolima2106 <pedrolima2106@gmail.com>
2026-01-13 23:05:49 -0300

    PostgreSQL conectado, Sequelize conectado

---
+++ src/config/database.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    logging: false
+  }
+);
+
+module.exports = sequelize;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 const app = require('./app');
+const sequelize = require('./config/database');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão com o banco OK 🚀');
+
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Erro ao conectar no banco:', error);
+  }
+})();
